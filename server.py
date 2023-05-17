c1d406e3579a405715469e10a0982b802786774a
JulianafBandeira <julianafbbarbosa@gmail.com>
2023-05-16 22:34:55 -0300

    conf host

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from remote_object import RemoteObject
 
 remote_obj = RemoteObject()
 
-daemon = Pyro4.Daemon()
+daemon = Pyro4.Daemon(host='192.168.100.104', port=9090)
 uri = daemon.register(remote_obj)
 
 print("URI do objeto remoto:", uri)
