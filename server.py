a496b3057dc6d8ef8c97451b8e511b00ba7049fd
JulianafBandeira <julianafbbarbosa@gmail.com>
2023-05-17 20:26:16 -0300

    atualizando

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ from remote_object import RemoteObject
 
 remote_obj = RemoteObject()
 
-daemon = Pyro4.Daemon(host='192.168.100.104', port=9090)
+'''Utilizar IP Wifi (testado em Ubuntu em diferentes redes (firewall desligado), porém, 
+resultando em [Errno 110] Connection timed out. Até certo ponto eles se enxergam; o server envia
+a função, mas na resposta do cliente o servidor não faz o retorno com a resposta esperada.
+Em terminais diferentes a comunicação ocorre.)'''
+
+daemon = Pyro4.Daemon(host='192.168.42.99', port=9090)
 uri = daemon.register(remote_obj)
 
 print("URI do objeto remoto:", uri)
